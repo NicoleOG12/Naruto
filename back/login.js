a615022b5e5d827ee37f7be8f9d5dca14f909316
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-05-12 16:58:44 -0300

    finalizando login

--- back/login.js
+++ back/login.js
@@ -1,24 +1,34 @@
-function get_name() {
-    let n = document.querySelector("#nome").value
-    console.log('Seu nome é: ')
+function dados() {
+    const ds = [
+        {id: 1, login: "nicole", senha: "1234", nome: "Nicole", email: "nicole@gmail.com"},
+        {id: 2, login: "julia", senha: "12345", nome: "Julia", email: "julia@gmail.com"},
+        {id: 3, login: "isabela", senha: "123456", nome: "Isabela", email: "isabela@gmail.com"},
+        {id: 3, login: "ana", senha: "1234567", nome: "Ana", email: "ana@gmail.com"}
+    ]
+    let json = JSON.stringify(ds)
+    localStorage.setItem('banco', json)
 }
 
-function login() {
+function logar() {
+    const ds = JSON.parse(localStorage.getItem('banco'))
+
     let lg = document.querySelector('#email').value
-    let sn = parseInt (document.querySelector('#senha').value)
+    let sn = document.querySelector('#senha').value
 
-    let dados = [
-                    {id:1, nome:"Nicole", login:"nicole", senha:"1234", email:"nicole@gmail.com" },
-                    {id:2, nome:"Júlia", login:"julia", senha:"5678", email:"julia@gmail.com" },
-                    {id:3, nome:"Isabella", login:"isabella", senha:"2468", email:"isabella@gmail.com" },
-                    {id:4, nome:"Ana", login:"ana", senha:"1357", email:"ana@gmail.com" }
-                ]
+    for (let i=0; i<ds.length; i++) {
+        if(lg == ds[i].email && sn == ds[i].senha) {
+            alert('Bem-vindo: '+ ds[i].nome)
 
-    for(let i=0; i < dados.length; i++){
-        if(lg == dados[i].email && sn == dados[i].senha) {
-            alert("Login feito com sucesso!")
+           localStorage.setItem('usuarioLogado', JSON.stringify(ds[i]));
+            window.location.href = 'home.html'; 
+            return;
+           
         }
     }
+    alert('Usuário ou senha inválidos.');
 }
 
-
+function logado() {
+    let usuario = sessionStorage.getItem('user')
+    document.querySelector('#usuario').value = usuario
+}
